e53daf36d229d5b2eab7d6c51963ecfdd9aee14f
Lesley <leskimuti@gmail.com>
2022-07-31 11:03:12 +0300

    Continuing with ES6JAvascript from Pirple

--- templeteliterals/main.js
+++ templeteliterals/main.js
@@ -77,3 +77,110 @@ const myPizzaDiv = `<article>
 console.log(myPizzaDiv);
 
 myDiv.innerHTML += myPizzaDiv;
+
+console.log(myDiv);
+// DESTRUCTURING
+
+/*
+
+  - The destructuring assignment syntax is a Javascript expression that makes it possible to extract data from arrays or objects into distinct variables
+  - Destructuring syntax can be used on left-hand side of assignment
+*/
+
+const myArrayNum = [1, 2, 3, 4, 5, 7, 7, 8];
+
+const [x, y, z] = myArrayNum;
+
+console.table(myArray);
+
+console.log(x, y, z);
+
+// Object Destructuring
+
+const myObj = {
+  firstName: "Chris",
+  lastName: "Houston",
+  age: 23,
+  height: "5'7''",
+};
+
+const { firstName, lastName, age, height } = myObj;
+// const { age, firstName } = myObj;
+
+console.log(firstName, lastName, age, height);
+// console.log(firstName, age);
+
+const myObj2 = {
+  title: "My address book",
+  entries: [
+    {
+      name: "Bob",
+      number: "0743458906",
+      address: "1572-30100, Eldoret",
+    },
+    {
+      name: "Chris",
+      number: "5433-677633",
+      address: "157 Main St",
+    },
+    {
+      name: "Lesley",
+      number: "4355-24567",
+      address: "30100 Broadway",
+    },
+  ],
+  myPhone: "2455-677878",
+};
+
+const { title, entries } = myObj2;
+
+const {
+  // title,
+  entries: [{ address, name: nnn }],
+} = myObj2;
+
+for (const { name, address } of entries) {
+  console.table(`Name: ${name}, address is: ${address}`);
+}
+
+console.log(nnn, address, title);
+
+// Exeption Handling
+
+// Throw your own exeption
+
+/*
+
+  -  Use the throw statement to throw own exeption
+  - You specify the exeption containing the value to be thrown 
+    e.g : throw expresion
+  -  Can throw any expression
+
+*/
+
+// throw "New Error!";
+
+function checkIfNum(n) {
+  if (isNaN(n)) {
+    throw "This is not a number";
+  } else {
+    console.log(n);
+  }
+}
+
+checkIfNum(" ");
+// const myObj3 = { a: 1, b: 2 };
+
+// myObj3.map((obj) => console.log(obj));
+
+function MyException(message) {
+  this.message = message;
+  this.name = "My Exeption";
+  this.toString = function () {
+    return this.name + ": " + this.message;
+  };
+}
+
+throw new MyException("Field Invalid");
+
+// try ....catch sdtatements
