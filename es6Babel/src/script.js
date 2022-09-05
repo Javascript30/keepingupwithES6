ded427496bba0a62b69207db1f0508d17eb00a2c
Lesley <leskimuti@gmail.com>
2022-09-05 12:44:45 +0300

    Added watcher to watch a source for es6 js

---
+++ es6Babel/src/script.js
@@ -0,0 +1,27 @@
+const myDiv = document.getElementById("main");
+
+const myArr = [1, 2, 3, 4, 5, 7, 8];
+
+for (const prop of myArr) {
+  myDiv.innerHTML += `<p>${prop}</p>`;
+}
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHi() {
+    console.log(`Hi, ${this.name}`);
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+const kim = new Person("Kimutai", 34);
+
+kim.sayHi();
+
+const sally = new Person("Sally", 16);
+sally.sayHi();
+sally.sayHi();
