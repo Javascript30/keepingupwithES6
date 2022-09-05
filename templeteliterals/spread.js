c6b8f1ea74d7a996180e004244f63c6aa4821a4b
Lesley <leskimuti@gmail.com>
2022-09-05 10:22:40 +0300

    Learning mapping

--- templeteliterals/spread.js
+++ templeteliterals/spread.js
@@ -12,3 +12,19 @@ function spreadFunction(...multipleArgs) {
 }
 
 spreadFunction(1, 20, true, "hey", "kim");
+
+// Map Object
+
+const myMap = new Map();
+
+myMap.set("key", "value");
+myMap.set("anotherKey", "anotherValue");
+myMap.set(0, "anotherValue2");
+
+console.log(myMap);
+console.log(myMap.size);
+
+// myMap.clear();
+
+console.log(myMap.get(0));
+console.log(myMap.has(2));
