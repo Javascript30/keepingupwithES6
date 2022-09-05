c1dd7e531110568f00e9b182caefafb6e04944a5
Lesley <leskimuti@gmail.com>
2022-09-05 12:36:07 +0300

    Added use of classes pre es6

--- es6Babel/script.js
+++ es6Babel/script.js
@@ -5,3 +5,17 @@ const myArr = [1, 2, 3, 4, 5, 7, 8];
 for (const prop of myArr) {
   myDiv.innerHTML += `<p>${prop}</p>`;
 }
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHi() {
+    console.log(`Hi, ${this.name}`);
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+const kim = new Person("Kimutai", 34);
