ca375bba8c7bc31d2e36bbf7a32dcecf90441b92
Lesley <leskimuti@gmail.com>
2022-09-07 12:58:49 +0300

    Learning more gulp usage

--- es6Babel/Gulpfile.js
+++ es6Babel/Gulpfile.js
@@ -1,6 +1,17 @@
 const gulp = require("gulp");
 const babel = require("gulp-babel");
 
+gulp.task("moveHTML", function() {
+  return gulp.src("src/*.html").pipe(gulp.dest("dist"));
+});
+
+// gulp.task("js", ["moveHTML"], function() {
+//   return gulp
+//     .src("src/*.js")
+//     .pipe(babel())
+//     .pipe(gulp.dest("dist"));
+// });
+
 gulp.task("js", function() {
   return gulp
     .src("src/*.js")
