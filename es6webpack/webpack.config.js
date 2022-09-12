e7fafcba0d16f12f07b3505a2fbbc6fc1e67d2ab
Lesley <leskimuti@gmail.com>
2022-09-12 18:59:34 +0300

    Configuring webpack to use

---
+++ es6webpack/webpack.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars, no-undef
+const path = require("path");
+
+
+// eslint-disable-next-line no-undef
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        path: 
+    }
+}
