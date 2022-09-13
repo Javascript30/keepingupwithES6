1c2b57d1e4b33500142f250e31c3ecff201560c7
Lesley <leskimuti@gmail.com>
2022-09-13 10:45:57 +0300

    Setting up webpack

--- es6webpack/webpack.config.js
+++ es6webpack/webpack.config.js
@@ -1,11 +1,21 @@
 // eslint-disable-next-line no-unused-vars, no-undef
 const path = require("path");
 
-
 // eslint-disable-next-line no-undef
 module.exports = {
-    entry: "./src/app.js",
-    output: {
-        path: 
-    }
-}
+  entry: "./src/app.js",
+  output: {
+    // eslint-disable-next-line no-undef
+    path: path.resolve(__dirname, "public"),
+    filename: "app.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
